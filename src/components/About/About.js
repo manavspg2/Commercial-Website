11b9848ea010e373e92b91c05de256403d7bfec1
Manav Trivedi <58290635+manavspg2@users.noreply.github.com>
2020-12-08 10:53:18 +0530

    Create About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is about page</h1>
+                <a href="https://raw.githubusercontent.com/BestBuyAPIs/open-data-set/master/categories.json" download>Download</a>
+            </div>
+        )
+    }
+}
