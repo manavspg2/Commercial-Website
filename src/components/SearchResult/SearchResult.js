23fc009864361604f50028a4d62816aabd40ad00
Manav Trivedi <58290635+manavspg2@users.noreply.github.com>
2020-12-08 11:24:10 +0530

    Create SearchResult.js

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,13 @@
+mport React, { Component } from "react";
+import ProductsList from './components/ProductsList.js'
+// import './script.js'
+
+export default class Search_result extends Component {
+	render() {
+		return (
+		<div id="Search">
+        <ProductsList/>
+		</div>
+		)
+	}
+}
