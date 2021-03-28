ccc3e374b1ed3398ab93d5d17a0321b599f9f7c3
Manav Trivedi <58290635+manavspg2@users.noreply.github.com>
2021-03-28 11:00:55 +0530

    Create 0004_auto_20210328_1408.py

---
+++ core/migrations/0004_auto_20210328_1408.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_userprofile'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userprofile',
+            name='one_click_purchasing',
+            field=models.BooleanField(default=False),
+        ),
+    ]
